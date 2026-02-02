9840294122d9baafdec382abaf3970a29c4b0675
KG070324 <zgpn0703@leeds.ac.uk>
2026-02-02 11:19:23 +0000

    on question3 Portfolio

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,3 +1,6 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
+select StudentId, FirstName, LastName, EnrolmentYear
+from student
+where EnrolmentYear = 2022
--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,3 +1,6 @@
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, DateOfBirth
+
+select StudentId, FirstName, LastName, DateOfBirth 
+from student order by DateOfBirth desc limit 5 
