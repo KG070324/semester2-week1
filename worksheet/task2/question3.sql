ebda4bd9a017f70ef8188f58fee25466a978ef60
KG070324 <zgpn0703@leeds.ac.uk>
2026-02-04 12:23:44 +0000

    Finished portfolio task

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,9 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+
+select d.DepartmentName, count(e.enrolmentid) as TotalEnrolments 
+from department d 
+join course c on d.departmentid = c.departmentid
+join enrolment e on e.courseid = c.courseid
+group by d.DepartmentName
--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,3 +1,9 @@
 -- List all students along with the names of the courses they are enrolled in.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
+
+select s.studentid , s.firstname, s.lastname, c.CourseName
+from student s 
+join enrolment e on e.studentid = s.studentid
+join course c on c.courseid = e.courseid 
+
